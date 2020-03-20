1bc0604c3dc434f41066e9fc2e428349cc4763b8
marcovirgolin <marco.virgolin@gmail.com>
2020-03-20 16:49:40 +0100

    updated setup version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='SimpleGP',
-    version='0.9.1',
+    version='0.9.2',
     author='Marco Virgolin',
     author_email='marco.virgolin@gmail.com',
     url='https://github.com/marcovirgolin/SimpleGP',
